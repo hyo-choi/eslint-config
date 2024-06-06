f10274b0e5ae7bc32e09b3f2d175c635bc3efc6d
Hyojeong Choi <gywjddl001@naver.com>
2024-06-06 17:01:45 +0900

    :heavy_plus_sign: Replace prettier config to the one from repo

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,3 @@
-/** @type {import("prettier").Config} */
-const config = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  semi: true,
-  quoteProps: "as-needed",
-  singleQuote: false,
-  jsxSingleQuote: false,
-  bracketSpacing: true,
-  bracketSameLine: false,
-};
+const config = require("@hyo-choi/prettier-config");
 
 module.exports = config;
